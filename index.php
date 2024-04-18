abae0b09e1b7140e6785e9414e3392e3e97ad7aa
zaydolas74 <r0934607@student.thomasmore.be>
2024-04-18 16:46:50 +0200

    :Change css/index

--- index.php
+++ index.php
@@ -11,7 +11,18 @@
     <?php
     ?>
     <div id="nav">
-        
+        <img src="images/Little-Sun-Logo.png" alt="">
     </div>
+
+    <div id="content">
+            <div>
+                <h1>Little Sun Shiftplanner</h1>
+                <p>Welcome to Little Sun Shiftplaner, the ultimate platform for shift planners in Zambia! At Little Sun Shiftpaner, we empower workers to take control of their schedules by defining their roles and selecting preferred work locations. Our user-friendly interface allows workers to plan their availability for shifts and even schedule well-deserved vacations with ease.</p>
+            </div>
+            <div>
+
+            </div>
+    </div>
+
 </body>
 </html>
