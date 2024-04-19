f9647828ea771204b111923bedb73cc82202d843
zaydolas74 <r0934607@student.thomasmore.be>
2024-04-20 01:48:09 +0200

    feat: frontend login

--- index.php
+++ index.php
@@ -1,28 +1,62 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Little Sun</title>
-    
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <?php
-    ?>
-    <div id="nav">
-        <img src="images/Little-Sun-Logo.png" alt="">
-    </div>
+<?php include_once 'bootstrap.php'; ?>
 
-    <div id="content">
-            <div>
-                <h1>Little Sun Shiftplanner</h1>
-                <p>Welcome to Little Sun Shiftplaner, the ultimate platform for shift planners in Zambia! At Little Sun Shiftpaner, we empower workers to take control of their schedules by defining their roles and selecting preferred work locations. Our user-friendly interface allows workers to plan their availability for shifts and even schedule well-deserved vacations with ease.</p>
+<!-- Section: Design Block -->
+<section class="">
+    <!-- Jumbotron -->
+    <div class="px-4 py-5 px-md-5 text-center text-lg-start">
+        <div class="container">
+            <div class="row gx-lg-5 align-items-center">
+                <div class="col-lg-6 mb-5 mb-lg-0">
+                    <h1 class="mb-5 display-4 ls-tight">
+                        Welcome to <span class="text-primary" style="font-weight: bold;">Little Sun</span> shiftplanner
+                    </h1>
+                    <p style="color: hsl(217, 10%, 50.8%)">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Eveniet, itaque accusantium odio, soluta, corrupti aliquam
+                        quibusdam tempora at cupiditate quis eum maiores libero
+                        veritatis? Dicta facilis sint aliquid ipsum atque?
+                    </p>
+                </div>
+
+                <div class="col-lg-6 mb-5 mb-lg-0">
+                    <div class="card">
+                        <div class="card-body py-4 px-5">
+                            <form>
+
+
+                                <!-- Email input -->
+                                <div data-mdb-input-init class="form-outline mb-4">
+                                    <label class="form-label" for="form3Example3">Email address</label>
+                                    <input type="email" id="form3Example3" class="form-control" />
+
+                                </div>
+
+                                <!-- Password input -->
+                                <div data-mdb-input-init class="form-outline mb-4">
+                                    <label class="form-label" for="form3Example4">Password</label>
+                                    <input type="password" id="form3Example4" class="form-control" />
+                                </div>
+
+                                <!-- Submit button -->
+                                <button type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block w-100">
+                                    <span style="font-weight: bold;">Sign in</span>
+                                </button>
+
+                            </form>
+                        </div>
+
+                        <div class="card-footer py-4 flex-column align-items-center justify-content-center">
+                            <div class="text-center ">
+                                <p>forgot your password?</p>
+                                <p>Don't have an account? <a href="#" style="font-weight: bold;">Sign up</a></p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
             </div>
-            <div>
-
-            </div>
-    </div>
-
-</body>
-</html>
+        </div>
+        <!-- Jumbotron -->
+</section>
+<!-- Section: Design Block -->
