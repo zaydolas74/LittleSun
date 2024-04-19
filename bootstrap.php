71d80d559a3bbdeca6c3ede4cd27ea55fa350de5
zaydolas74 <r0934607@student.thomasmore.be>
2024-04-20 01:47:00 +0200

    feat: bootstrapp build

--- bootstrap.php
+++ bootstrap.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Little Sun</title>
-    
+
     <!-- Include Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
-    
-    <!-- Link to your custom CSS file -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Include CSS -->
     <link rel="stylesheet" href="css/style.css">
+
 </head>
+
 <body>
