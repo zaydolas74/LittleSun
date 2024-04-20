d5653f507868d381f5d390d634cdac1959c3b4d7
zaydolas74 <r0934607@student.thomasmore.be>
2024-04-20 02:29:33 +0200

    feat: nav build

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php include_once 'bootstrap.php'; ?>
-
-<!-- Section: Design Block -->
-<section class="">
-    <!-- Jumbotron -->
-    <div class="px-4 py-5 px-md-5 text-center text-lg-start">
+<nav class="navbar navbar-light bg-dark py-3">
+    <div class="container justify-content-center">
+        <a class="navbar-brand" href="#">
+            <img src="images/Little-Sun-Logo.png" alt="" height="35">
+        </a>
+    </div>
+</nav>
+<section class="py-5">
+    <div class="px-4 py-5 px-md-5  text-lg-start">
         <div class="container">
             <div class="row gx-lg-5 align-items-center">
                 <div class="col-lg-6 mb-5 mb-lg-0">
@@ -22,41 +26,28 @@
                     <div class="card">
                         <div class="card-body py-4 px-5">
                             <form>
-
-
-                                <!-- Email input -->
                                 <div data-mdb-input-init class="form-outline mb-4">
                                     <label class="form-label" for="form3Example3">Email address</label>
                                     <input type="email" id="form3Example3" class="form-control" />
-
                                 </div>
-
-                                <!-- Password input -->
                                 <div data-mdb-input-init class="form-outline mb-4">
                                     <label class="form-label" for="form3Example4">Password</label>
                                     <input type="password" id="form3Example4" class="form-control" />
                                 </div>
-
-                                <!-- Submit button -->
                                 <button type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block w-100">
                                     <span style="font-weight: bold;">Sign in</span>
                                 </button>
-
                             </form>
                         </div>
-
                         <div class="card-footer py-4 flex-column align-items-center justify-content-center">
                             <div class="text-center ">
                                 <p>forgot your password?</p>
                                 <p>Don't have an account? <a href="#" style="font-weight: bold;">Sign up</a></p>
                             </div>
-
                         </div>
                     </div>
 
                 </div>
             </div>
         </div>
-        <!-- Jumbotron -->
 </section>
-<!-- Section: Design Block -->
